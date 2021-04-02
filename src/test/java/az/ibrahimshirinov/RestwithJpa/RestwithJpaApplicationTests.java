a8b01ce9a0c1db2c1cf24367272b1764b24dbd72
Ibrahim Shirinov <ibrahim.shirinovv@gmail.com>
2021-04-02 11:27:19 +0400

    first commit

---
+++ src/test/java/az/ibrahimshirinov/RestwithJpa/RestwithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ibrahimshirinov.RestwithJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
